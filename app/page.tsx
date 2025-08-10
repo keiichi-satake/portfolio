d5750f0d7eb2e4d330d1832105f08fbae0e4c6df
Studio Moz-YA <>
2025-08-10 15:46:28 +0900

    タイトル変更

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <main style={{ padding: '2rem' }}>
-      <h1>さたけ さんのポートフォリオ</h1>
+      <h1>さたぼーのポートフォリオ</h1>
       <p>ここに作品一覧や自己紹介などを表示していきます。</p>
     </main>
   );
